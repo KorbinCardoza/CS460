bd876a6ba2cbe2be5a06d98668236901c3412570
Korbin Cardoza <kcardoza17@wou.edu>
2021-09-29 19:08:53 -0700

    Completed Project

---
+++ HW1/CS460_HW1/CS460_HW1/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using CS460_HW1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS460_HW1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string Circumference, string Front, string Rear,string RPM,string Speed)
+        {
+            string[] frontGears = Front.Split(',');
+            string[] rearGears = Rear.Split(',');
+            List<double> speeds = new List<double>();
+
+            if (Speed == "MPH")
+            {
+                for (int i = 0; i < frontGears.Length; ++i)
+                {
+                    for (int j = 0; j < rearGears.Length; ++j)
+                    {
+                        speeds.Add( (Convert.ToDouble(RPM) *
+                                         (Convert.ToDouble(frontGears[i]) / Convert.ToDouble(rearGears[j])) *
+                                         Convert.ToDouble(Circumference) * 60 /
+                                         1000000) / 1.609);
+                    }
+                }
+            }
+
+            if (Speed == "KPH")
+            {
+
+                for (int i = 0; i < frontGears.Length; ++i)
+                {
+                    for (int j = 0; j < rearGears.Length; ++j)
+                    {
+                        speeds.Add(Convert.ToDouble(RPM) *
+                                   (Convert.ToDouble(frontGears[i]) / Convert.ToDouble(rearGears[j])) *
+                                   Convert.ToDouble(Circumference) * 60 /
+                                   1000000);
+                    }
+                }
+            }
+            ViewData["fronts"] = frontGears;
+            ViewData["rears"] = rearGears;
+            return View(speeds);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HW1/CS460_HW1/CS460_HW1/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    output.innerHTML = this.value;
+    
+    var element = document.getElementsByTagName("td");
+    for (var j = 0; j < element.length; ++j) {
+        element[j].style.color = 'black';
+            element[j].style.fontWeight = 'normal';
+    }
+    for (var k = 0; k < element.length; ++k) {
+        if (parseInt(element[k].id) > this.value) {
+            element[k].style.color = 'green';
+            element[k].style.fontWeight = 'bold';
+        }
+    }
+
+    }
+
+
